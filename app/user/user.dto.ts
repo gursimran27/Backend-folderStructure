78fbf17935474e083ef1ebc91923f3cece31ff7e
Gursimran27 <singhgursimran165@gmail.com>
2025-02-08 09:04:40 +0530

    first

---
+++ app/user/user.dto.ts
@@ -0,0 +1,14 @@
+import { type BaseSchema } from "../common/dto/base.dto";
+
+// export enum UserRole {
+//   ADMIN = "ADMIN",
+//   USER = "USER",
+// }
+
+export interface IUser extends BaseSchema {
+  name: string;
+  email: string;
+  active?: boolean;
+  role: "ADMIN" | "USER";
+  password: string;
+}
